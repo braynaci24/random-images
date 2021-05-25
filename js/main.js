e14dcf1071ddd80266a3cc630baba01c44314524
braynaci24 <aynacimuhammetemin@gmail.com>
2021-05-25 18:57:30 +0300

    general

--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $.ajax({
-         url: 'https://api.unsplash.com/photos/random?count=30&orientation=squarish&client_id=kfKv2eE4uFO3dEnB4lw7PlWZg6sZVc70qTbRRGDAfnU', 
+        url: 'https://api.unsplash.com/photos/random?count=30&orientation=squarish&client_id=kfKv2eE4uFO3dEnB4lw7PlWZg6sZVc70qTbRRGDAfnU', 
         method: 'GET',
         dataType: 'JSON',
         success: function (data) {
@@ -13,4 +13,20 @@ $(document).ready(function(){
      $('body').on('click', '.delete', function(){
          $(this).parent().parent().parent().remove();
      })
+
+     $('.fav').click(function(){
+         $('.favorites-container').show();
+     })
+
+     $('.fav-container-close-icon').click(function(){
+        $('.favorites-container').hide();
+     })
+
+     $('body').on('click', '.check', function(){
+       let imageSrc =  $(this).parent().parent().prev().attr('src')
+        for(let i = 0; i < $('.favorites-container').length; i++){
+          $('.favorites-container').append(`<img  class="fav-images" src="${imageSrc}"/>`)
+        }
+     })
+    
 })
